89e628d89c280ac87506c5650a08f7f4f41cd856
tauzn-clock <zhongdx@i2r.a-star.edu.sg>
2024-08-15 01:52:41 +0000

    migrate over

---
+++ scripts/custom_utils/data_conversion.py
@@ -0,0 +1,13 @@
+from PIL import Image
+import io
+import numpy as np
+
+def topic_to_image(msg):
+    img_raw = np.frombuffer(msg.data, dtype=np.uint8)
+    img = img_raw.reshape((msg.height, msg.width, 3))
+    return img
+
+def topic_to_depth(msg):
+    depth_raw = np.frombuffer(msg.data, dtype=np.uint16)
+    depth = depth_raw.reshape((msg.height, msg.width, 1))
+    return depth
